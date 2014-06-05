17ae40a5b26c0979d6e2e943f3f2e4267e02e967
Hack Reactor Students <accounts@hackreactor.com>
2014-06-05 10:17:44 -0700

    Finished Prototypal Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue._storage = {};
+  queue._front = 0;
+  queue._back = 0;
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this._storage[this._front] = value;
+    this._front++;
+  },
+  dequeue: function(){
+    if ( this._front - this._back ) {
+      var result = this._storage[this._back];
+      delete this._storage[this._back];
+      this._back++;
+      return result;
+    }
+    return 0;
+  },
+  size: function(){
+    return this._front - this._back;
+  }
+};
+
 
 
