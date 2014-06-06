d576f9e4ec5b80685bb472e8a7c7f6f98845fa0c
Hack Reactor Students <nhbottomley@gmail.com>
2014-06-06 16:20:28 -0700

    Finish set extras

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -14,8 +14,27 @@ describe('set', function() {
   it('should add values to a set', function(){
     set.add("Susan Sarandon");
     set.add("Danny Glover");
+    set.add("'Hello'");
     expect(set.contains('Danny Glover')).to.equal(true);
     expect(set.contains('Susan Sarandon')).to.equal(true);
+    expect(set.contains("'Hello'")).to.equal(true);
+
+  });
+
+  it('should add numbers to a set', function(){
+    set.add(1234567890);
+    expect(set.contains(1234567890)).to.equal(true);
+  });
+
+  it('should add objects to a set', function(){
+    var obj = { name: "whatever", value: "something" };
+    set.add(obj);
+    expect(set.contains(obj)).to.equal(true);
+  });
+
+  it('should check if it contains a value', function(){
+    set.add("blah");
+    expect(set.contains("blah")).to.equal(true);
   });
 
   it('should remove values from a set', function(){
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,24 +1,27 @@
 var makeSet = function(){
   var set = Object.create(setPrototype);
-  set._storage = Object.create(null);
+  set._storage = Object.create(null); {}
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
-  if ( !(item in this._storage) ) {
-    this._storage[item] = true;
+  var key = JSON.stringify(item);
+  if ( !(key in this._storage) ) {
+    this._storage[key] = true;
   }
 };
 
 setPrototype.contains = function(item){
-  return !!(this._storage[item]);
+  var key = JSON.stringify(item);
+  return !!(this._storage[key]);
 };
 
 setPrototype.remove = function(item){
-  if ( this.contains( item ) ) {
-    delete this._storage[item];
+  var key = JSON.stringify(item);
+  if ( this.contains(item) ) {
+    delete this._storage[key];
   }
 };
 
