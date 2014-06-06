d4ea04d99d1831294fa533991a67dd7b3fcf70ca
Hack Reactor Students <nhbottomley@gmail.com>
2014-06-06 13:17:31 -0700

    Refactoring bst and tree recursion

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,22 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  if ( this._storage[i] ) {
+    // collision
+  }
+
+  this._storage[i] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage[i];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = null;
 };
 
 
